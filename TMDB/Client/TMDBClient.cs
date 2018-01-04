2246625600c9334253cf1ce572588c5a5d839254
Steeven Diard <steevenbrest@hotmail.com>
2018-01-05 00:04:08 +0100

    SDD - Ajout des notions de client TMDB

---
+++ TMDB/Client/TMDBClient.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMDB.Client
+{
+    public class TMDBClient
+    {
+        private String ApiVersion;
+        private String BaseURL;
+
+        private readonly JsonSerializer Serializer;
+    }
+}
